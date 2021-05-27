d599986ec99e8350d87a03d2d3b7820fd7a879f2
umparadoxo <legiaored@gmail.com>
2021-05-27 12:57:42 -0300

    Primeiros arquivos
    arquivos do formulário versão 1

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from '@material-ui/core'
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Container component='article' maxWidth='sm'>
+        <Typography
+          variant='h3'
+          component='h1'
+          align='center'
+        >
+          Formulário de Cadastro
+        </Typography>
+
+        <FormularioCadastro
+          aoEnviar={aoEnviarForm}
+          validarCPF = {validarCPF}
+        />
+
+      </Container>
+    );
+  }
+}
+
+function validarCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: 'O CPF deve ter 11 dígitos' }
+  } else {
+    return { valido: true, texto: '' }
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+export default App;
